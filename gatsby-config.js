4965075569f95c719fe3d8bd3619656de8b8c916
Adam Powell <adampowell@adams-imac.home>
2020-02-28 09:33:11 +0000

    Updating security settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,6 +7,22 @@ module.exports = {
   },
   plugins: [
     {
+      resolve: `gatsby-plugin-csp`,
+      options: {
+        disableOnDev: true,
+        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+        mergeScriptHashes: true, // you can disable scripts sha256 hashes
+        mergeStyleHashes: true, // you can disable styles sha256 hashes
+        mergeDefaultDirectives: true,
+        directives: {
+          "script-src": "'self' www.developeradam.com",
+          "style-src": "'self' 'unsafe-inline'",
+          "img-src": "'self' data: www.developeradam.com",
+          // you can add your directives or override defaults
+        },
+      },
+    },
+    {
       resolve: "gatsby-source-sanity",
       options: {
         projectId: "7v5vogcz",
